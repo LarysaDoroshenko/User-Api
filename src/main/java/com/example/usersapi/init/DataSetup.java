068e2c926ae2efb3dd5ce4d52f7e6db5431ffeb4
Larysa Doroshenko <larysad785@gmail.com>
2021-03-05 02:35:57 +0200

    feat: introduce config server

--- src/main/java/com/example/usersapi/init/DataSetup.java
+++ src/main/java/com/example/usersapi/init/DataSetup.java
@@ -3,12 +3,10 @@ package com.example.usersapi.init;
 import com.example.usersapi.entity.UserEntity;
 import com.example.usersapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
-@Profile("dev")
 @Component
 @RequiredArgsConstructor
 public class DataSetup {
